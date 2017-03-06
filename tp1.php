062a5ac522a06a8096d94cb65faab27b8c9a51de
Neto Da Silva André <andre.netodasi@he-arc.ch>
2017-03-06 22:38:56 +0100

    Création du fichier tp1.php qui contiendra le Labo 1

---
+++ tp1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>TP 1 Algo</title>
+  </head>
+  <body>
+    <h1>Codage et décodage binaire</h1>
+    <!-- A faire -->
+  </body>
+</html>
