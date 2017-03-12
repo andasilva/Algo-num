7fff3027da5b9b35c2fc563f0e4f2b8459d958ef
Neto da Silva André <andre.netodasilva@he-arc.ch>
2017-03-12 16:57:08 +0100

    Version with a new disposition of inputs.

--- tp1.js
+++ tp1.js
@@ -1,35 +1,23 @@
 
-//Fonction qui abrège document.getElementById
-function $(id) {
-    return document.getElementById(id);
-}
+/*************************/
+/* Fonctions principales */
+/*************************/
 
 function floatDecimalToBinaire()
 {
     var floatDecimal = $("chiffreDecimal").value;
-    var nBits = $("nbBits").value;
-    
+    var nBits = $("nbBits1").value;
+
     var signe = 0;
     var exposant = 0;
     var exposantBinaire;
     var mantisse;
     var mantisseBinaire;
 
-    if (floatDecimal && nBits)
+    if (floatDecimal && nBits && nBits>0)
     {
         //Calcul du nombre de bits de l'exposant et de la mantisse
-        var tailleExposant;
-        switch (nBits) {
-            case '16':
-                tailleExposant = 5;
-                break;
-            case '32':
-                tailleExposant = 8;
-                break;
-            default:
-                tailleExposant = Math.round(4 * Math.log2(nBits)) - 13;
-                break;
-        }
+        var tailleExposant = calculTailleExposant(nBits);
         var tailleMantisse = nBits - tailleExposant - 1;
 
         //Valeur du signe
@@ -55,72 +43,64 @@ function floatDecimalToBinaire()
         exposantBinaire = decimalToBinaire(exposant, tailleExposant);
 
         //Calcul de la mantisse
-        mantisse = (nombreDecimal - 1) * Math.pow(2, tailleMantisse);
+        mantisse = (floatDecimal - 1) * Math.pow(2, tailleMantisse);
 
         //Conversion de la mantisse en binaire
         mantisseBinaire = decimalToBinaire(mantisse, tailleMantisse);
 
         //Affichage des valeur
-        $("exposantBinaire").setAttribute('maxlength', tailleExposant);
-        $("mantisseBinaire").setAttribute('maxlength', tailleMantisse);
-
-        $("signeBinaire").value = signe;
-        $("exposantBinaire").value = exposantBinaire;
-        $("mantisseBinaire").value = mantisseBinaire;
+        $("resultatSigne").innerHTML = signe;
+        $("resultatExposant").innerHTML = exposantBinaire;
+        $("resultatMantisse").innerHTML = mantisseBinaire;
     }
-
 }
 
 
 function floatBinaireToDecimal()
 {
-    var nBits = $("nbBits").value;
-    var exposantTaille = 0;
-    var bitTemp = nBits;
-
-
-    while (bitTemp >= 2)
-    {
-        bitTemp = bitTemp / 2;
-        exposantTaille++;
-    }
-    exposantTaille = exposantTaille - 3;
-
-    var tailleExposant = exposantTaille * 3 + 2;
-    var tailleMantisse = nBits - tailleExposant - 1;
-
-    var nombreDecimal;
+    var nBits = $("nbBits2").value;
     var signeBinaire = $("signeBinaire").value;
     var exposantBinaire = $("exposantBinaire").value;
+    var exposantTaille = calculTailleExposant(nBits);
+    var exposantValeur;
     var mantisseBinaire = $("mantisseBinaire").value;
+    var mantisseTaille = nBits - exposantTaille -1;
+    var mantisseValeur;
+    
+    //calcul de la valeur de l'exposant et de la mantisse 
+    exposantValeur = binaireToDecimal(exposantBinaire, exposantTaille)-(Math.pow(2, exposantTaille-1) - 1);
+    mantisseValeur = binaireToDecimal(mantisseBinaire,mantisseTaille) / Math.pow(2,mantisseTaille)+1;
+    
+    //Calcul du résultat final
+    var resultat = (-2 * signeBinaire +1) * Math.pow(2, exposantValeur) * mantisseValeur; // signe * 2^exposant * mantisse
 
-    var exposantDecimal = binaireToDecimal(exposantBinaire, tailleExposant);
-    var mantisseDecimal = binaireToDecimal(mantisseBinaire, tailleMantisse);
-
-    mantisseDecimal = mantisseDecimal + Math.pow(2, tailleMantisse);
-    mantisseDecimal = mantisseDecimal / Math.pow(2, tailleMantisse);
-
-    exposantDecimal = exposantDecimal - (Math.pow(2, tailleExposant - 1) - 1);
-
-    nombreDecimal = Math.pow(2, exposantDecimal) * mantisseDecimal;
-
-    if (signeBinaire == "1")
-    {
-        nombreDecimal = (-nombreDecimal);
-    }
-
-    $("chiffreDecimal").value = nombreDecimal;
-
-
-
+    $("resultatFloat").innerHTML = resultat;
 }
 
-/**
- * Retourne un String contenant la valeur binaire d'un chiffre.
- * @param {int} nombreDecimal Le chiffre à convertir
- * @param {int} nbBit Le nombre de bits qui sera utilisé pour coder le chiffre
- * @returns {String} Le chiffre converti en decimal selon nbBit bits
- */
+/*************************/
+/* Fonctions secondaires */
+/*************************/
+
+function $(id) {
+    return document.getElementById(id);
+}
+
+function calculTailleExposant(numberOfBits){
+    let tailleExposant;
+        switch (numberOfBits) {
+            case '16':
+                tailleExposant = 5;
+                break;
+            case '32':
+                tailleExposant = 8;
+                break;
+            default:
+                tailleExposant = Math.round(4 * Math.log2(nBits)) - 13;
+                break;
+        }
+    return tailleExposant;
+}
+
 function decimalToBinaire(nombreDecimal, nbBit)
 {
     var nombreBinaire = "";
@@ -138,8 +118,7 @@ function binaireToDecimal(nombreBinaire, nbBit)
 {
     var nombreDecimal = 0;
     for (let i = 0; i < nbBit; i++) {
-        nombreDecimal += nombreBinaire[i] * Math.pow(2, i);
+        nombreDecimal += nombreBinaire[nbBit-i-1] * Math.pow(2, i);
     }
     return nombreDecimal;
-
 }
--- tp1.php
+++ tp1.php
@@ -1,52 +1,87 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <title>TP 1 Algo</title>
-</head>
-<body>
-  <script src="tp1.js"></script>
-  <script src="tp1-prelabo.js"></script>
-  <h1>Codage et décodage binaire</h1>
-  <h2>Pré-labo</h2>
-  <!--
-  <form action="javascript:void(0)">
-  1er nombre: <input type="number" name="nbr1Decimal" value="0" id="nbr1Decimal"
-  onchange="DisplayNumberToBinary('nbr1Decimal', 'nbr1Binaire')"><br>
-  <span id="negatifValue1">
-  négatif: <input type="checkbox" name="negatif" class="negatif" ><br>
-</span>
-en binaire:
-<span id="nbr1Binaire">
-<input type="checkbox" name="bit0" class="bit0" value="bit0" onchange="updateNumberFromBinary('nbr1Binaire', 'nbr1Decimal')">
-</span><br>
-<button type="button" onclick="addByte('nbr1Binaire', 'nbr1Decimal')"> ajouter un byte</button><br>
-
-2ème nombre: <input type="number" name="nbr2Decimal" id="nbr2Decimal" value="0" onchange="DisplayNumberToBinary('nbr2Decimal', 'nbr2Binaire')"><br>
-en binaire: <span id="nbr2Binaire"><input type="checkbox" name="bit0" class="bit0" value="bit0" onchange="updateNumberFromBinary('nbr2Binaire', 'nbr2Decimal')"></span><br>
-<button type="button" onclick="addByte('nbr2Binaire', 'nbr2Decimal')"> Ajouter un byte</button><br><br>
-
-<input type="submit" name="add" value="Addition" onclick="addTwoBinaryNumber('nbr1Binaire', 'nbr2Binaire','reponseBinaire','reponse')">
-<input type="submit" name="add" value="Soustraction">
-<input type="submit" name="add" value="Multiplication">
-<br>
-Résultat:
-<input type="number" name="reponse" id="reponse"><br>
-<br>en binaire:
-<span id="reponseBinaire">
-
-</span>
-</form>
--->
-<h2>Labo</h2>
-<form action="javascript:void(0)">
-  Nombre de bits : <input type="text" value="32" name="nbBits" class="nbBits" id="nbBits" onchange="floatDecimalToBinaire()" />
-  <br/>
-  Chiffre à convertir: <input type="text" name="chiffreDecimal" class="chiffreDecimal" id="chiffreDecimal" onchange="floatDecimalToBinaire()"/>
-  <br/>
-  <input type="text" maxlength="1" name="signeBinaire" class="signeBinaire"  id="signeBinaire" onchange="floatBinaireToDecimal()"/>
-  <input type="text" name="exposantBinaire" class="exposantBinaire"  id="exposantBinaire" onchange="floatBinaireToDecimal()"/>
-  <input type="text" name="mantisseBinaire" class="mantisseBinaire"  id="mantisseBinaire" onchange="floatBinaireToDecimal()"/>
-</form>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <title>TP 1 Algo</title>
+    </head>
+    <body>
+        <script src="tp1.js"></script>
+        <script src="tp1-prelabo.js"></script>
+        <h1>Codage et décodage binaire (IEEE 754)</h1>
+        
+        <!--
+        <h2>Pré-labo</h2>
+        <form action="javascript:void(0)">
+        1er nombre: <input type="number" name="nbr1Decimal" value="0" id="nbr1Decimal"
+        onchange="DisplayNumberToBinary('nbr1Decimal', 'nbr1Binaire')"><br>
+        <span id="negatifValue1">
+        négatif: <input type="checkbox" name="negatif" class="negatif" ><br>
+      </span>
+      en binaire:
+      <span id="nbr1Binaire">
+      <input type="checkbox" name="bit0" class="bit0" value="bit0" onchange="updateNumberFromBinary('nbr1Binaire', 'nbr1Decimal')">
+      </span><br>
+      <button type="button" onclick="addByte('nbr1Binaire', 'nbr1Decimal')"> ajouter un byte</button><br>
+      
+      2ème nombre: <input type="number" name="nbr2Decimal" id="nbr2Decimal" value="0" onchange="DisplayNumberToBinary('nbr2Decimal', 'nbr2Binaire')"><br>
+      en binaire: <span id="nbr2Binaire"><input type="checkbox" name="bit0" class="bit0" value="bit0" onchange="updateNumberFromBinary('nbr2Binaire', 'nbr2Decimal')"></span><br>
+      <button type="button" onclick="addByte('nbr2Binaire', 'nbr2Decimal')"> Ajouter un byte</button><br><br>
+      
+      <input type="submit" name="add" value="Addition" onclick="addTwoBinaryNumber('nbr1Binaire', 'nbr2Binaire','reponseBinaire','reponse')">
+      <input type="submit" name="add" value="Soustraction">
+      <input type="submit" name="add" value="Multiplication">
+      <br>
+      Résultat:
+      <input type="number" name="reponse" id="reponse"><br>
+      <br>en binaire:
+      <span id="reponseBinaire">
+      
+      </span>
+      </form>
+        -->
+        <h2>Labo</h2>
+        <form action="javascript:void(0)">
+            <table border="1" width="100%">
+                <thead>
+                    <tr>
+                        <th colspan="2" width="50%">Float en Binaire</th>
+                        <th colspan="2" width="50%">Binaire en Float</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td width="25%">Nombre de bits</td>
+                        <td><input type="text" value="32" name="nbBits" id="nbBits1" onchange="floatDecimalToBinaire()" /></td>
+                        <td width="25%">Nombre de bits</td>
+                        <td><input type="text" value="32" name="nbBits" id="nbBits2" onchange="floatDecimalToBinaire()" /></td>
+                    </tr>
+                    <tr>
+                        <td>Chiffre à convertir</td>
+                        <td><input type="text" name="chiffreDecimal" id="chiffreDecimal" onchange="floatDecimalToBinaire()"/></td>
+                        <td>Valeur Binaire à convertir</td>
+                        <td>
+                            <input type="text" maxlength="1" name="signeBinaire" class="signeBinaire"  id="signeBinaire" onchange="floatBinaireToDecimal()"/>
+                            <input type="text" name="exposantBinaire" class="exposantBinaire"  id="exposantBinaire" onchange="floatBinaireToDecimal()"/>
+                            <input width="100%" type="text" name="mantisseBinaire" class="mantisseBinaire"  id="mantisseBinaire" onchange="floatBinaireToDecimal()"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td width="25%">Résultat:</td>
+                        <td>
+                            <span id="resultatSigne"></span>
+                            <span id="resultatExposant"></span>
+                            <span id="resultatMantisse"></span>  
+                        </td>
+                        <td>Résultat</td>
+                        <td>
+                            <div id="resultatFloat"></div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+        <footer>
+            Labo réalisé par: Jmaa Mohamed, Castella Killian, Neto da Silva André & Piquerez Thibaut
+        </footer>
+    </body>
 </html>
